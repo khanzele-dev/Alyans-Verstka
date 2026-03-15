9756c99b78c413b01b7c25f83b765e5f9c9ffaee
khanzele16 <khanzele16@gmail.com>
2026-03-15 15:37:17 +0300

    Four Commit

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 import "./styles/normalize.scss";
 import "./styles/components/header.scss"
 import "./styles/pages/development.scss"
+
+document.querySelectorAll('#calc-area, #calc-land').forEach((input) => {
+  input.addEventListener('input', () => {
+    input.value = input.value.replace(/\D/g, '');
+  });
+});
